b78b3d8040d001db1472fd9f55e9e0601e530799
Vishal Dabhi <vishaldabhi23@gmail.com>
2023-11-08 00:06:50 +0530

    router setup done

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,4 +1,4 @@
 const GOOGLE_VIDEO_API_KEY = process.env.REACT_APP_API_KEY;
 export const YOUTUBE_VIDEO_API =
-  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US&key=" +
+  "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
   GOOGLE_VIDEO_API_KEY;
